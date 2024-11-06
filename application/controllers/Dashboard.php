d5ebd04bac6154be83db15d43631178cc09fd73d
alfaturachman <122202202929@mhs.dinus.ac.id>
2024-11-06 10:50:38 +0700

    initial commit

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->load->view('backend/partials/header');
+        $this->load->view('backend/dashboard');
+        $this->load->view('backend/partials/footer');
+    }
+}
---
+++ application/views/backend/renstra/view.php
@@ -0,0 +1,82 @@
+<main id="main" class="main">
+    <div class="pagetitle">
+        <h1>General Tables</h1>
+        <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item">Tables</li>
+                <li class="breadcrumb-item active">General</li>
+            </ol>
+        </nav>
+    </div>
+    <!-- End Page Title -->
+
+    <section class="section">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Default Table</h5>
+                        <!-- Default Table -->
+                        <table class="table table-bordered">
+                            <thead class="table-primary">
+                                <tr class="text-center align-middle">
+                                    <th scope="col" rowspan="2" style="width: 50px;">No</th>
+                                    <th scope="col" rowspan="2" colspan="2" style="width: 50px;">Butir</th>
+                                    <th scope="col" rowspan="2" style="width: 200px;">Indikator Kinerja Utama</th>
+                                    <th scope="col" colspan="6" style="width: 300px;">Tahun</th>
+                                </tr>
+                                <tr class="text-center align-middle">
+                                    <th scope="col">2021</th>
+                                    <th scope="col">2022</th>
+                                    <th scope="col">2023</th>
+                                    <th scope="col">2024</th>
+                                    <th scope="col">2025</th>
+                                    <th scope="col">2026</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td colspan="8">Menghasilkan lulusan yang berkualitas dibidangnya dan berjiwa wirausaha.</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <th scope="row">1.1.</th>
+                                    <td colspan="8">Terselenggaranya Good University Governance.</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <th></th>
+                                    <th scope="row">1.1.1.</th>
+                                    <td>Akreditasi APT-BAN PT </td>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <th></th>
+                                    <th scope="row">1.1.2.</th>
+                                    <td colspan="8">Jumlah pengelola/unit tersertifikasi/terakreditasi oleh lembaga nasional/internasional</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <th></th>
+                                    <th class="text-end" scope="row">1.1.2.a.</th>
+                                    <td class="text-end">Perpustakaan</td>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <!-- End Default Table Example -->
+                    </div>
+                </div>
