148417fb7dd82fedba662bce55b66e5cd5722317
alfaturachman <122202202929@mhs.dinus.ac.id>
2024-11-28 14:16:40 +0700

    fix chart

--- application/views/backend/ewmp/hasil.php
+++ application/views/backend/ewmp/hasil.php
@@ -1,3 +1,9 @@
+<style>
+    div.dt-container select.dt-input {
+        margin-right: 8px;
+    }
+</style>
+
 <main id="main" class="main">
     <div class="pagetitle">
         <nav>
@@ -21,6 +27,29 @@
                         </div>
                         <div class="d-flex justify-content-between">
                             <a href="<?= base_url('renstra/generate_pdf') ?>" type="button" target="_blank" class="btn btn-danger mb-4">Cetak PDF</a>
+                        </div>
+                        <div class="row">
+                            <div class="col-6" style="height: 300px;">
+                                <div id="chartInternasional"></div>
+                            </div>
+                            <div class="col-6">
+                                <table class="table table-bordered">
+                                    <tbody>
+                                        <tr>
+                                            <td>Internasional</td>
+                                            <td>22</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Nasional</td>
+                                            <td>11</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Total</td>
+                                            <td>33</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                         <div class="table-responsive">
                             <table class="table table-bordered" id="datatable">
@@ -77,7 +106,60 @@
 </main>
 
 <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 <script>
     new DataTable('#datatable');
+
+    var options = {
+        series: [25, 15],
+        chart: {
+            width: '100%',
+            height: '100%',
+            type: 'pie',
+        },
+        labels: ['Internasinal', 'Nasional'],
+        theme: {
+            monochrome: {
+                enabled: true,
+            },
+        },
+        plotOptions: {
+            pie: {
+                dataLabels: {
+                    offset: -5,
+                },
+            },
+        },
+        grid: {
+            padding: {
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+            },
+        },
+        dataLabels: {
+            formatter(val, opts) {
+                const name = opts.w.globals.labels[opts.seriesIndex];
+                return [name, val.toFixed(1) + '%'];
+            },
+        },
+        legend: {
+            show: true, // Menampilkan legend
+            position: 'bottom', // Posisi legend di bawah
+            horizontalAlign: 'center', // Rata tengah secara horizontal
+            markers: {
+                width: 12,
+                height: 12,
+                radius: 12, // Membuat marker berbentuk lingkaran
+            },
+            itemMargin: {
+                horizontal: 10,
+                vertical: 5,
+            },
+        },
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chartInternasional"), options);
+    chart.render();
 </script>
--- application/views/backend/ewmp/hasil_views/publikasi_internasional.php
+++ application/views/backend/ewmp/hasil_views/publikasi_internasional.php
@@ -19,12 +19,43 @@
         <div class="row">
             <div class="col">
                 <div class="card">
-                    <div class="card-header text-white bg-primary">
+                    <div class="card-header text-white bg-success">
                         <h5 class="pt-2"><strong>Daftar Publikasi Internasional EWMP</strong></h5>
                     </div>
                     <div class="card-body">
-                        <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
                             Silahkan untuk mengecek Publikasi Internasional Pelaporan EWMP Fakultas Teknik UDINUS Semarang
+                        </div>
+                        <div class="row">
+                            <div class="col-6" style="height: 300px;">
+                                <div id="chartQ"></div>
+                            </div>
+                            <div class="col-6">
+                                <table class="table table-bordered">
+                                    <tbody>
+                                        <tr>
+                                            <td>Q1</td>
+                                            <td>22</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Q2</td>
+                                            <td>11</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Q3</td>
+                                            <td>33</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Q4</td>
+                                            <td>33</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Total</td>
+                                            <td>33</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                         <div class="table-responsive">
                             <table class="table" id="datatable">
@@ -52,8 +83,8 @@
                                                     <?php endforeach; ?>
                                                 <?php endif; ?>
                                             </td>
-                                            <td class="align-middle"><?= $pi->judul_artikel?></td>
-                                            <td class="align-middle"><?= $pi->judul_jurnal?></td>
+                                            <td class="align-middle"><?= $pi->judul_artikel ?></td>
+                                            <td class="align-middle"><?= $pi->judul_jurnal ?></td>
                                             <td class="align-middle"><?= formatDateTime($pi->ins_time) ?></td>
                                             <td class="align-middle">
                                                 <a href="<?= site_url('ewmp/detail_pelaporan/' . htmlspecialchars($pi->id)) ?>" class="btn btn-sm btn-success">
@@ -83,9 +114,62 @@
 </main>
 
 <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 <script>
     new DataTable('#datatable');
+
+    var options = {
+        series: [25, 15, 44, 55],
+        chart: {
+            width: '100%',
+            height: '100%',
+            type: 'pie',
+        },
+        labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+        theme: {
+            monochrome: {
+                enabled: true,
+            },
+        },
+        plotOptions: {
+            pie: {
+                dataLabels: {
+                    offset: -5,
+                },
+            },
+        },
+        grid: {
+            padding: {
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+            },
+        },
+        dataLabels: {
+            formatter(val, opts) {
+                const name = opts.w.globals.labels[opts.seriesIndex];
+                return [name, val.toFixed(1) + '%'];
+            },
+        },
+        legend: {
+            show: true, // Menampilkan legend
+            position: 'bottom', // Posisi legend di bawah
+            horizontalAlign: 'center', // Rata tengah secara horizontal
+            markers: {
+                width: 12,
+                height: 12,
+                radius: 12, // Membuat marker berbentuk lingkaran
+            },
+            itemMargin: {
+                horizontal: 10,
+                vertical: 5,
+            },
+        },
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chartQ"), options);
+    chart.render();
 </script>
 
 <?php
