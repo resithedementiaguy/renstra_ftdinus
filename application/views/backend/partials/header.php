bbfd072362bbf412950cfaf7ff058a135a89ff0b
alfaturachman <122202202929@mhs.dinus.ac.id>
2024-12-09 18:18:02 +0700

    fix error

--- application/views/backend/partials/header.php
+++ application/views/backend/partials/header.php
@@ -87,7 +87,7 @@
                         <li>
                             <a
                                 class="dropdown-item d-flex align-items-center"
-                                href="profil">
+                                href="<?= base_url('') ?>profil">
                                 <i class="bi bi-person"></i>
                                 <span>Profil Saya</span>
                             </a>
