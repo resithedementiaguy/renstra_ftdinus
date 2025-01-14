f9bdccbd520b4b2d4257d5604c3395f9eef83a55
alfaturachman <122202202929@mhs.dinus.ac.id>
2025-01-14 10:45:40 +0700

    fix ewmp edit haki

--- application/controllers/Ewmp.php
+++ application/controllers/Ewmp.php
@@ -1892,7 +1892,6 @@ class Ewmp extends CI_Controller
             'nama_pemegang' => $this->input->post('nama_pemegang'),
             'judul' => $this->input->post('judul'),
             'sertifikat' => $this->input->post('sertifikat'),
-            'tahun' => $this->input->post('tahun'),
         );
 
         // Remove null or empty fields
@@ -1928,7 +1927,6 @@ class Ewmp extends CI_Controller
             'nama_pemegang' => $this->input->post('nama_pemegang'),
             'judul' => $this->input->post('judul'),
             'sertifikat' => $this->input->post('sertifikat'),
-            'tahun' => $this->input->post('tahun'),
         );
 
         // Remove null or empty fields
@@ -1947,6 +1945,110 @@ class Ewmp extends CI_Controller
         } catch (Exception $e) {
             // Set error flash message
             $this->session->set_flashdata('error', 'Gagal memperbarui data HAKI Merk: ' . $e->getMessage());
+
+            redirect('ewmp/edit_haki/' . $id_pelaporan);
+        }
+    }
+
+    public function update_haki_paten($id_haki)
+    {
+        // Get related id_pelaporan for redirection
+        $id_pelaporan = $this->Ewmp_model->get_id_pelaporan_by_haki_id($id_haki);
+
+        // Get post data
+        $update_data = array(
+            'prodi' => $this->input->post('prodi'),
+            'nama_usul' => $this->input->post('nama_usul'),
+            'nama_inventor' => $this->input->post('nama_inventor'),
+            'judul' => $this->input->post('judul'),
+            'sertifikat' => $this->input->post('sertifikat'),
+        );
+
+        // Remove null or empty fields
+        $update_data = array_filter($update_data, function ($value) {
+            return ($value !== null && $value !== '');
+        });
+
+        try {
+            // Update the record in haki_paten table
+            $this->Ewmp_model->update_haki_paten($id_haki, $update_data);
+
+            // Set success flash message
+            $this->session->set_flashdata('success', 'Data HAKI Paten berhasil diperbarui.');
+
+            redirect('ewmp/detail_pelaporan/' . $id_pelaporan);
+        } catch (Exception $e) {
+            // Set error flash message
+            $this->session->set_flashdata('error', 'Gagal memperbarui data HAKI Paten: ' . $e->getMessage());
+
+            redirect('ewmp/edit_haki/' . $id_pelaporan);
+        }
+    }
+
+    public function update_haki_dindustri($id_haki)
+    {
+        // Get related id_pelaporan for redirection
+        $id_pelaporan = $this->Ewmp_model->get_id_pelaporan_by_haki_id($id_haki);
+
+        // Get post data
+        $update_data = array(
+            'prodi' => $this->input->post('prodi'),
+            'nama_usul' => $this->input->post('nama_usul'),
+            'judul' => $this->input->post('judul'),
+            'sertifikat' => $this->input->post('sertifikat'),
+        );
+
+        // Remove null or empty fields
+        $update_data = array_filter($update_data, function ($value) {
+            return ($value !== null && $value !== '');
+        });
+
+        try {
+            // Update the record in haki_dindustri table
+            $this->Ewmp_model->update_haki_dindustri($id_haki, $update_data);
+
+            // Set success flash message
+            $this->session->set_flashdata('success', 'Data HAKI Desain Industri berhasil diperbarui.');
+
+            redirect('ewmp/detail_pelaporan/' . $id_pelaporan);
+        } catch (Exception $e) {
+            // Set error flash message
+            $this->session->set_flashdata('error', 'Gagal memperbarui data HAKI Desain Industri: ' . $e->getMessage());
+
+            redirect('ewmp/edit_haki/' . $id_pelaporan);
+        }
+    }
+
+    public function update_haki_buku($id_haki)
+    {
+        // Get related id_pelaporan for redirection
+        $id_pelaporan = $this->Ewmp_model->get_id_pelaporan_by_haki_id($id_haki);
+
+        // Get post data
+        $update_data = array(
+            'prodi' => $this->input->post('prodi'),
+            'nama_usul' => $this->input->post('nama_usul'),
+            'isbn' => $this->input->post('isbn'),
+            'judul_buku' => $this->input->post('judul_buku'),
+            'file_buku' => $this->input->post('file_buku'),
+        );
+
+        // Remove null or empty fields
+        $update_data = array_filter($update_data, function ($value) {
+            return ($value !== null && $value !== '');
+        });
+
+        try {
+            // Update the record in haki_dindustri table
+            $this->Ewmp_model->update_haki_buku($id_haki, $update_data);
+
+            // Set success flash message
+            $this->session->set_flashdata('success', 'Data HAKI Desain Industri berhasil diperbarui.');
+
+            redirect('ewmp/detail_pelaporan/' . $id_pelaporan);
+        } catch (Exception $e) {
+            // Set error flash message
+            $this->session->set_flashdata('error', 'Gagal memperbarui data HAKI Desain Industri: ' . $e->getMessage());
 
             redirect('ewmp/edit_haki/' . $id_pelaporan);
         }
--- application/views/backend/ewmp/edit/edit_haki.php
+++ application/views/backend/ewmp/edit/edit_haki.php
@@ -171,6 +171,173 @@
                             </div>
                         </form>
                     <?php
+                    } elseif ($haki['kategori'] == "Paten") {
+                    ?>
+                        <form action="<?= base_url('ewmp/update_haki_paten/' . $haki['id']) ?>" method="post">
+                            <div class="card-body">
+                                <div class="row mb-3">
+                                    <label for="nama_usul" class="col-sm-2 col-form-label">Nama Pengusul</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="nama_usul" name="nama_usul" value="<?= htmlspecialchars($haki['paten_nama_usul']) ?>" required>
+                                    </div>
+                                </div>
+
+                                <fieldset class="row mb-3">
+                                    <legend class="col-form-label col-sm-2 pt-0">Program Studi Ketua</legend>
+                                    <div class="col-sm-10">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="prodi" id="prodi1" value="Teknik Elektro" <?= isset($haki['paten_prodi']) && $haki['paten_prodi'] === 'Teknik Elektro' ? 'checked' : ''; ?>>
+                                            <label class="form-check-label" for="prodi1">Teknik Elektro</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="prodi" id="prodi2" value="Teknik Industri" <?= isset($haki['paten_prodi']) && $haki['paten_prodi'] === 'Teknik Industri' ? 'checked' : ''; ?>>
+                                            <label class="form-check-label" for="prodi2">Teknik Industri</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="prodi" id="prodi3" value="Teknik Biomedis" <?= isset($haki['paten_prodi']) && $haki['paten_prodi'] === 'Teknik Biomedis' ? 'checked' : ''; ?>>
+                                            <label class="form-check-label" for="prodi3">Teknik Biomedis</label>
+                                        </div>
+                                    </div>
+                                </fieldset>
+
+                                <div class="row mb-3">
+                                    <label for="judul" class="col-sm-2 col-form-label">Judul</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="judul" name="judul" value="<?= htmlspecialchars($haki['paten_judul']) ?>" required>
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <label for="sertifikat" class="col-sm-2 col-form-label">Sertifikat</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="sertifikat" name="sertifikat" value="<?= htmlspecialchars($haki['paten_sertifikat']) ?>" required>
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <label for="nama_inventor" class="col-sm-2 col-form-label">Nama Inventor</label>
+                                    <div class="col-sm-10">
+                                        <textarea class="form-control" id="nama_inventor" name="nama_inventor" rows="3" required><?= htmlspecialchars($haki['paten_nama_inventor']) ?></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <div class="d-flex justify-content-between">
+                                    <a href="<?= base_url('ewmp/detail_pelaporan/' . $haki['id_pelaporan']) ?>" class="btn btn-secondary">Kembali</a>
+                                    <button type="submit" class="btn btn-success">Simpan Perubahan</button>
+                                </div>
+                            </div>
+                        </form>
+                    <?php
+                    } elseif ($haki['kategori'] == "Desain Industri") {
+                    ?>
+                        <form action="<?= base_url('ewmp/update_haki_dindustri/' . $haki['id']) ?>" method="post">
+                            <div class="card-body">
+                                <div class="row mb-3">
+                                    <label for="nama_usul" class="col-sm-2 col-form-label">Nama Pengusul</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="nama_usul" name="nama_usul" value="<?= htmlspecialchars($haki['dindustri_nama_usul']) ?>" required>
+                                    </div>
+                                </div>
+
+                                <fieldset class="row mb-3">
+                                    <legend class="col-form-label col-sm-2 pt-0">Program Studi Ketua</legend>
+                                    <div class="col-sm-10">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="prodi" id="prodi1" value="Teknik Elektro" <?= isset($haki['dindustri_prodi']) && $haki['dindustri_prodi'] === 'Teknik Elektro' ? 'checked' : ''; ?>>
+                                            <label class="form-check-label" for="prodi1">Teknik Elektro</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="prodi" id="prodi2" value="Teknik Industri" <?= isset($haki['dindustri_prodi']) && $haki['dindustri_prodi'] === 'Teknik Industri' ? 'checked' : ''; ?>>
+                                            <label class="form-check-label" for="prodi2">Teknik Industri</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="prodi" id="prodi3" value="Teknik Biomedis" <?= isset($haki['dindustri_prodi']) && $haki['dindustri_prodi'] === 'Teknik Biomedis' ? 'checked' : ''; ?>>
+                                            <label class="form-check-label" for="prodi3">Teknik Biomedis</label>
+                                        </div>
+                                    </div>
+                                </fieldset>
+
+                                <div class="row mb-3">
+                                    <label for="judul" class="col-sm-2 col-form-label">Judul</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="judul" name="judul" value="<?= htmlspecialchars($haki['dindustri_judul']) ?>" required>
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <label for="sertifikat" class="col-sm-2 col-form-label">Sertifikat</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="sertifikat" name="sertifikat" value="<?= htmlspecialchars($haki['dindustri_sertifikat']) ?>" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <div class="d-flex justify-content-between">
+                                    <a href="<?= base_url('ewmp/detail_pelaporan/' . $haki['id_pelaporan']) ?>" class="btn btn-secondary">Kembali</a>
+                                    <button type="submit" class="btn btn-success">Simpan Perubahan</button>
+                                </div>
+                            </div>
+                        </form>
+                    <?php
+                    } elseif ($haki['kategori'] == "Buku") {
+                    ?>
+                        <form action="<?= base_url('ewmp/update_haki_buku/' . $haki['id']) ?>" method="post" enctype="multipart/form-data">
+                            <div class="card-body">
+                                <div class="row mb-3">
+                                    <label for="nama_usul" class="col-sm-2 col-form-label">Nama Pengusul</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="nama_usul" name="nama_usul" value="<?= htmlspecialchars($haki['buku_nama_usul']) ?>" required>
+                                    </div>
+                                </div>
+
+                                <fieldset class="row mb-3">
+                                    <legend class="col-form-label col-sm-2 pt-0">Program Studi Ketua</legend>
+                                    <div class="col-sm-10">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="prodi" id="prodi1" value="Teknik Elektro" <?= isset($haki['buku_prodi']) && $haki['buku_prodi'] === 'Teknik Elektro' ? 'checked' : ''; ?>>
+                                            <label class="form-check-label" for="prodi1">Teknik Elektro</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="prodi" id="prodi2" value="Teknik Industri" <?= isset($haki['buku_prodi']) && $haki['buku_prodi'] === 'Teknik Industri' ? 'checked' : ''; ?>>
+                                            <label class="form-check-label" for="prodi2">Teknik Industri</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="prodi" id="prodi3" value="Teknik Biomedis" <?= isset($haki['buku_prodi']) && $haki['buku_prodi'] === 'Teknik Biomedis' ? 'checked' : ''; ?>>
+                                            <label class="form-check-label" for="prodi3">Teknik Biomedis</label>
+                                        </div>
+                                    </div>
+                                </fieldset>
+
+                                <div class="row mb-3">
+                                    <label for="isbn" class="col-sm-2 col-form-label">ISBN</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="isbn" name="isbn" value="<?= htmlspecialchars($haki['buku_isbn']) ?>" required>
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <label for="judul_buku" class="col-sm-2 col-form-label">Judul Buku</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="judul_buku" name="judul_buku" value="<?= htmlspecialchars($haki['buku_judul_buku']) ?>" required>
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <label for="file_buku" class="col-sm-2 col-form-label">File Buku</label>
+                                    <div class="col-sm-10">
+                                        <input type="file" class="form-control" id="file_buku" name="file_buku">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <div class="d-flex justify-content-between">
+                                    <a href="<?= base_url('ewmp/detail_pelaporan/' . $haki['id_pelaporan']) ?>" class="btn btn-secondary">Kembali</a>
+                                    <button type="submit" class="btn btn-success">Simpan Perubahan</button>
+                                </div>
+                            </div>
+                        </form>
+                    <?php
                     }
                     ?>
                 </div>
@@ -189,7 +356,7 @@
             <div class="modal-body text-center py-4">
                 <i class="bi bi-check-circle-fill text-success" style="font-size: 3rem;"></i>
                 <h4 class="mt-3">Berhasil!</h4>
-                <p class="mb-0">Data editor jurnal berhasil diperbarui.</p>
+                <p class="mb-0">Data HAKI berhasil diperbarui.</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
@@ -198,27 +365,7 @@
     </div>
 </div>
 
-<!-- Update your existing script tag with this new version -->
 <script>
-    function formatRupiah(input) {
-        // Remove all non-numeric characters
-        let rawValue = input.value.replace(/[^\d]/g, '');
-
-        // Store the raw value in hidden input
-        document.getElementById('besar_hibah_raw').value = rawValue;
-
-        // Format the display value
-        if (rawValue) {
-            let number = parseInt(rawValue);
-            let formatted = new Intl.NumberFormat('id-ID').format(number);
-            input.value = 'Rp ' + formatted;
-        } else {
-            input.value = '';
-        }
-    }
-
-
-    // Initialize modal if it hasn't been initialized
     $(document).ready(function() {
         // Make sure Bootstrap Modal is properly initialized
         var successModal = new bootstrap.Modal(document.getElementById('successModal'), {
